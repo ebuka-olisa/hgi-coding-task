1ac798859d18e38c957cc9b86321bf8d461c1dca
Chukwuebuka <olisaemekaebuka@gmail.com>
2023-02-19 12:46:57 +0000

    Added method to get number of sequences in FASTQ file

--- FASTQProcessor.cs
+++ FASTQProcessor.cs
@@ -2,9 +2,25 @@ namespace HGICodingTask
 {
     public class FASTQProcessor
     {
+        // Calculates the number of sequences in a FASTQ file
         public static int NumberOfSequences(string filePath){
-            int sequenceCount = 0;
-            return sequenceCount;
+            int nonBlankLineCounter = 0;
+
+            // Read the file one line at a time
+            // This helps with memory management
+            foreach(string line in File.ReadLines(filePath))
+            {
+                // increase this count if the line found is not blank
+                if(!string.IsNullOrWhiteSpace(line))
+                {
+                    nonBlankLineCounter++;
+                }
+            }
+
+            if(nonBlankLineCounter > 0)
+                return nonBlankLineCounter / 4;
+            else
+                return 0;
         }
 
         public static int NumberOfNucleotides(string filePath){
