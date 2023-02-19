65f5618bf20472e882becdc395e433947869c1c3
Chukwuebuka <olisaemekaebuka@gmail.com>
2023-02-19 12:39:15 +0000

    Created methods and sample files for automatic testing

---
+++ FASTQProcessor.cs
@@ -0,0 +1,15 @@
+namespace HGICodingTask
+{
+    public class FASTQProcessor
+    {
+        public static int NumberOfSequences(string filePath){
+            int sequenceCount = 0;
+            return sequenceCount;
+        }
+
+        public static int NumberOfNucleotides(string filePath){
+            int nucleotideCount = 0;
+            return nucleotideCount;
+        }
+    }
+}
